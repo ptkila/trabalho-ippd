e54e03c74dc3574659b055d3e5b255c7351cf0d9
Thainan Remboski <naniaht@bitbucket.org>
2015-11-16 19:20:59 -0600

    Resolvi rodar 10 vezes cada caso de teste e fazer uma media pra pegar os tempos mais precisos. O execute.sh foi modificado pra rodar 10 vezes cada caso, dai eu criei um parser pra fazer a media e ja criei a tabela com as medias tambem.

--- Tarefa_C/execute.sh
+++ Tarefa_C/execute.sh
@@ -1,7 +1,19 @@
 for i in 1 2 4 8 16 32 64
 do
-	time ./out 10000 $i
-	time ./out 100000 $i
-	time ./out 1000000 $i
-	time ./out 10000000 $i
+	for j in {1..10}
+	do
+		time ./out 10000 $i
+	done
+	for j in {1..10}
+	do
+		time ./out 100000 $i
+	done
+	for j in {1..10}
+	do
+		time ./out 1000000 $i
+	done
+	for j in {1..10}
+	do
+		time ./out 10000000 $i
+	done
 done
--- Tarefa_C/parser_media.py
+++ Tarefa_C/parser_media.py
@@ -15,17 +15,43 @@ def main(argv):
 		f1.write('\t10000\t\t\t100000\t\t\t1000000\t\t\t10000000')
 
 		t = 0
+		t1 = '10000'
+		soma = 0.0;
+		cont = 0
+		flag = False
 		for line in lines:
 			if(line.startswith('THREAD')):
 				num_thread = line.split(' ')[1]
 				if(num_thread != t):
 					f1.write('\n' + num_thread + '\t')
 					t = num_thread
+					flag = True
+				# else:
+				# 	f1.write('\t\t')
+			elif(line.startswith('TOTAL')):
+				total = line.split(' ')[1]
+				if(total != t1):
+					soma = 0.0
+					cont = 1
+					# if(not flag):
+					# 	f1.write('\t\t\t')
+					# 	flag = False
+					t1 = total
 				else:
-					f1.write('\t\t')
+					cont += 1
 			elif(line.startswith('real')):
 				time = line.split()[1]
-				f1.write(time)
+				time = time.split('m')[1]
+				time = time.split('s')[0]
+				time = float(time)
+
+				soma += time
+				# print soma
+
+				if(cont == 10):
+					media = soma / 10.0
+					# print type(media)
+					f1.write(str(media) + 's\t\t\t')
 			else:
 				pass
 
@@ -33,6 +59,6 @@ def main(argv):
 
 	except Exception, e1:
 		print e1
-		
+
 if __name__ == '__main__':
 	main(sys.argv[1:])
